27b415a5bbe54ffa2ff6ff354b37e9ab9fe10c72
Kenichi Ishigaki <ishigaki@cpan.org>
2011-03-26 11:58:15 +0900

    initial import

---
+++ lib/App/ppmminus.pm
@@ -0,0 +1,35 @@
+package App::ppmminus;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::ppmminus - yet another PPM client
+
+=head1 SYNOPSIS
+
+  $ ppmm install App-ppmminus
+
+=head1 DESCRIPTION
+
+To be written later.
+
+=head1 AUTHOR
+
+Kenichi Ishigaki, E<lt>ishigaki@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Kenichi Ishigaki.
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
